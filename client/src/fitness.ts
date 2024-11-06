76afd9d4e88ffec8daca801d69967c48bf82e254
xVexit <pkalisz66@gmail.com>
2024-11-06 21:06:15 +0100

    Updated fitness function interface

--- client/src/fitness.ts
+++ client/src/fitness.ts
@@ -1,19 +1,24 @@
+import * as WebGL from "./common/webgl.ts"
+
 /**
  * Creates a fitness calculation function for a specified population. The fitness
  * will be calculated by rendering images of specified width and height and
  * comparing them to a target image. The fitness function takes in an array of
  * vertices and computes the fitness values for the population.
  *
- * @param {WebGLRenderingContext} context - The WebGL context used for rendering.
+ * @param {WebGL.Context} context - The WebGL context used for rendering.
  * @param {number} width - The width of the image in pixels.
  * @param {number} height - The height of the image in pixels.
- * @param {number} population - The population.
+ * @param {number} triangles - The number of triangles per single invidual.
+ * @param {number} population - The number of inviduals.
+ * 
  * @returns {function(Float32Array): Float32Array} - The fitness function.
  */
 export function createFitnessFunction(
-    context: WebGLRenderingContext,
+    context: WebGL.Context,
     width: number,
     height: number,
+    triangles: number,
     population: number,
 ): (vertices: Float32Array) => Float32Array {
     // TODO: Implement the rendering and fitness calculation
