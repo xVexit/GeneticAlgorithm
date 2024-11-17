d918d262b6ac7b95344e654315db21b78db5e668
xVexit <pkalisz66@gmail.com>
2024-11-17 18:40:56 +0100

    Added fintess calculation function using "compute" shader

--- client/src/fitness.ts
+++ client/src/fitness.ts
@@ -1,9 +1,10 @@
-import * as WebGL from "./common/webgl.ts"
+import * as WebGL from "./common/webgl.ts";
+import { FRAGMENT_SHADER_FITNESS, VERTEX_SHADER_FITNESS } from "./shaders.ts";
 
 /**
  * Creates a fitness calculation function for a specified population. The fitness
  * will be calculated by rendering images of specified width and height and
- * comparing them to a target image. The fitness function takes in an array of
+ * comparing them to a reference image. The fitness function takes in an array of
  * vertices and computes the fitness values for the population.
  *
  * @param {WebGL.Context} context - The WebGL context used for rendering.
@@ -11,7 +12,8 @@ import * as WebGL from "./common/webgl.ts"
  * @param {number} height - The height of the image in pixels.
  * @param {number} triangles - The number of triangles per single invidual.
  * @param {number} population - The number of inviduals.
- * 
+ * @param {WebGL.Texture} reference - The WebGL texture of the reference image.
+ *
  * @returns {function(Float32Array): Float32Array} - The fitness function.
  */
 export function createFitnessFunction(
@@ -22,4 +24,138 @@ export function createFitnessFunction(
     population: number,
 ): (vertices: Float32Array) => Float32Array {
     // TODO: Implement the rendering and fitness calculation
+}
+
+export function createFitnessComputeFunction(
+    context: WebGL.Context,
+    width: number,
+    height: number,
+    population: number,
+    reference: WebGL.Texture,
+    framebuffer?: WebGL.Framebuffer,
+): WebGL.ResourceWithDeleteFunction<(generated: WebGL.Texture) => void> {
+    const [vertexShader, deleteVertexShader] = WebGL
+        .createShader(
+            context,
+            WebGL.VERTEX_SHADER,
+            VERTEX_SHADER_FITNESS,
+        );
+
+    const [fragmentShader, deleteFragmentShader] = WebGL
+        .createShader(
+            context,
+            WebGL.FRAGMENT_SHADER,
+            FRAGMENT_SHADER_FITNESS,
+        );
+
+    const [shaderProgram, deleteShaderProgram] = WebGL
+        .createProgram(
+            context,
+            [
+                vertexShader,
+                fragmentShader,
+            ],
+        );
+
+    const uniforms = {
+        texture_reference: WebGL
+            .selectUniform(context, shaderProgram, "texture_reference"),
+        texture_generated: WebGL
+            .selectUniform(context, shaderProgram, "texture_generated"),
+    };
+
+    const [vertexBuffer, deleteVertexBuffer] = WebGL
+        .createBuffer(
+            context,
+            new Float32Array([
+                -1.0,
+                -1.0,
+                1.0,
+                -1.0,
+                1.0,
+                1.0,
+
+                -1.0,
+                -1.0,
+                1.0,
+                1.0,
+                -1.0,
+                1.0,
+            ]),
+        );
+
+    const [vertexArray, deleteVertexArray] = WebGL
+        .createVertexArray(
+            context,
+            [
+                {
+                    index: 0,
+                    size: 2,
+                    buffer: vertexBuffer,
+                    offset: 0,
+                    stride: 2 * Float32Array.BYTES_PER_ELEMENT,
+                },
+            ],
+        );
+
+    return [
+        (generated: WebGL.Texture): void => {
+            WebGL.drawArrays(
+                context,
+                shaderProgram,
+                vertexArray,
+                6,
+                {
+                    framebuffer: framebuffer,
+                    viewport: {
+                        x: 0,
+                        y: 0,
+                        width: width * population,
+                        height: 1,
+                    },
+                    clear: {
+                        color: {
+                            r: 0,
+                            g: 0,
+                            b: 0,
+                            a: 1,
+                        },
+                    },
+                    textures: [
+                        {
+                            unit: 0,
+                            texture: reference,
+                        },
+                        {
+                            unit: 1,
+                            texture: generated,
+                        },
+                    ],
+                    uniforms: [
+                        {
+                            type: WebGL.UNIFORM_INT,
+                            location: uniforms.texture_reference,
+                            value: {
+                                x: 0,
+                            },
+                        },
+                        {
+                            type: WebGL.UNIFORM_INT,
+                            location: uniforms.texture_generated,
+                            value: {
+                                x: 1,
+                            },
+                        },
+                    ],
+                },
+            );
+        },
+        (): void => {
+            deleteVertexShader();
+            deleteFragmentShader();
+            deleteVertexArray();
+            deleteVertexBuffer();
+            deleteShaderProgram();
+        },
+    ];
 }
--- client/src/shaders.ts
+++ client/src/shaders.ts
@@ -23,3 +23,43 @@ void main() {
     color = fragment_color;
 }
 `;
+
+export const VERTEX_SHADER_FITNESS = SHADER_VERSION + `
+layout(location = 0) in vec2 vertex_position;
+
+void main() {
+    gl_Position = vec4(vertex_position, 0.0, 1.0);
+}
+`;
+
+export const FRAGMENT_SHADER_FITNESS = SHADER_VERSION + `
+precision mediump float;
+
+layout(location = 0) out vec4 color;
+
+uniform sampler2D texture_reference;
+uniform sampler2D texture_generated;
+
+vec3 colorDifferenceAverage(in vec2 pixel, in ivec2 dimensions) {
+    vec4 total = vec4(0.0, 0.0, 0.0, 0.0);
+
+    for(int y = 0; y < dimensions.y; y++) {
+        total += abs(
+            texelFetch(texture_reference, ivec2(pixel.x, y), 0)
+                - texelFetch(texture_generated, ivec2(pixel.x, y), 0)
+        );
+    }
+
+    return total.xyz / float(dimensions.y);
+}
+
+void main() {
+    color = vec4(
+        colorDifferenceAverage(
+            gl_FragCoord.xy,
+            textureSize(texture_generated, 0)
+        ), 
+        1.0
+    );
+}
+`;
