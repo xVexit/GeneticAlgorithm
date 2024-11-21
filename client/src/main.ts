df426f5723e53f51a0e5685ee008c6e0c4cd492c
xVexit <pkalisz66@gmail.com>
2024-11-21 14:28:01 +0100

    Added event handling for sliders

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,1 +1,78 @@
-console.log("Hello World!");
+function setSliderUpdateCallback(
+  query: string,
+  updateCallback: (value: number) => void,
+): void {
+  const slider = document.querySelector(query) as HTMLInputElement | null;
+  if (slider) {
+    updateCallback(parseFloat(slider.value));
+    slider.addEventListener("input", (event: Event) => {
+      const value = (event.target as HTMLInputElement | null)?.value;
+      if (value) {
+        updateCallback(parseFloat(value));
+      }
+    });
+  }
+}
+
+function setupPopulationSlider(): void {
+  const title = document.querySelector("#slider-population-title") as
+    | HTMLHeadingElement
+    | null;
+  setSliderUpdateCallback(
+    "#slider-population",
+    (value: number) => {
+      if (title) {
+        title.innerText = `POPULATION (${value || 1})`;
+      }
+    },
+  );
+}
+
+function setupTrianglesSlider(): void {
+  const title = document.querySelector("#slider-triangles-title") as
+    | HTMLHeadingElement
+    | null;
+  setSliderUpdateCallback(
+    "#slider-triangles",
+    (value: number) => {
+      if (title) {
+        title.innerText = `TRIANGLES (${value || 1})`;
+      }
+    },
+  );
+}
+
+function setupMutationSlider(): void {
+  const title = document.querySelector("#slider-mutation-title") as
+    | HTMLHeadingElement
+    | null;
+  setSliderUpdateCallback(
+    "#slider-mutation",
+    (value: number) => {
+      if (title) {
+        title.innerText = `MUTATION (${value})`;
+      }
+    },
+  );
+}
+
+function setupResolutionSlider(): void {
+  const title = document.querySelector("#slider-resolution-title") as
+    | HTMLHeadingElement
+    | null;
+  setSliderUpdateCallback(
+    "#slider-resolution",
+    (value: number) => {
+      if (title) {
+        title.innerText = `RESOLUTION (${value}x${value})`;
+      }
+    },
+  );
+}
+
+self.addEventListener("load", () => {
+  setupPopulationSlider();
+  setupTrianglesSlider();
+  setupMutationSlider();
+  setupResolutionSlider();
+});
