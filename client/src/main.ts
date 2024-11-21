38b3b89aa8f50bad3fdb227e6754af5c990386b9
xVexit <pkalisz66@gmail.com>
2024-11-21 18:50:09 +0100

    Added reference image loading functionality

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,4 +1,19 @@
-function setSliderUpdateCallback(
+import * as WebGL from "./common/webgl.ts";
+
+const SLIDER_POPULATION: string = "#slider-population";
+const SLIDER_POPULATION_TITLE: string = "#slider-population-title";
+const SLIDER_TRIANGLES: string = "#slider-triangles";
+const SLIDER_TRIANGLES_TITLE: string = "#slider-triangles-title";
+const SLIDER_MUTATION: string = "#slider-mutation";
+const SLIDER_MUTATION_TITLE: string = "#slider-mutation-title";
+const SLIDER_RESOLUTION: string = "#slider-resolution";
+const SLIDER_RESOLUTION_TITLE: string = "#slider-resolution-title";
+
+const CONTENT_IMAGE_UPLOAD: string = "#content-image-upload";
+const CONTENT_IMAGE_REFERENCE: string = "#content-image-reference";
+const CONTENT_IMAGE_GENERATED: string = "#content-image-generated";
+
+function setupSliderUpdateCallback(
   query: string,
   updateCallback: (value: number) => void,
 ): void {
@@ -14,12 +29,12 @@ function setSliderUpdateCallback(
   }
 }
 
-function setupPopulationSlider(): void {
-  const title = document.querySelector("#slider-population-title") as
+function setupPopulationSliderElement(): void {
+  const title = document.querySelector(SLIDER_POPULATION_TITLE) as
     | HTMLHeadingElement
     | null;
-  setSliderUpdateCallback(
-    "#slider-population",
+  setupSliderUpdateCallback(
+    SLIDER_POPULATION,
     (value: number) => {
       if (title) {
         title.innerText = `POPULATION (${value || 1})`;
@@ -28,12 +43,12 @@ function setupPopulationSlider(): void {
   );
 }
 
-function setupTrianglesSlider(): void {
-  const title = document.querySelector("#slider-triangles-title") as
+function setupTrianglesSliderElement(): void {
+  const title = document.querySelector(SLIDER_TRIANGLES_TITLE) as
     | HTMLHeadingElement
     | null;
-  setSliderUpdateCallback(
-    "#slider-triangles",
+  setupSliderUpdateCallback(
+    SLIDER_TRIANGLES,
     (value: number) => {
       if (title) {
         title.innerText = `TRIANGLES (${value || 1})`;
@@ -42,12 +57,12 @@ function setupTrianglesSlider(): void {
   );
 }
 
-function setupMutationSlider(): void {
-  const title = document.querySelector("#slider-mutation-title") as
+function setupMutationSliderElement(): void {
+  const title = document.querySelector(SLIDER_MUTATION_TITLE) as
     | HTMLHeadingElement
     | null;
-  setSliderUpdateCallback(
-    "#slider-mutation",
+  setupSliderUpdateCallback(
+    SLIDER_MUTATION,
     (value: number) => {
       if (title) {
         title.innerText = `MUTATION (${value})`;
@@ -56,12 +71,12 @@ function setupMutationSlider(): void {
   );
 }
 
-function setupResolutionSlider(): void {
-  const title = document.querySelector("#slider-resolution-title") as
+function setupResolutionSliderElement(): void {
+  const title = document.querySelector(SLIDER_RESOLUTION_TITLE) as
     | HTMLHeadingElement
     | null;
-  setSliderUpdateCallback(
-    "#slider-resolution",
+  setupSliderUpdateCallback(
+    SLIDER_RESOLUTION,
     (value: number) => {
       if (title) {
         title.innerText = `RESOLUTION (${value}x${value})`;
@@ -70,9 +85,86 @@ function setupResolutionSlider(): void {
   );
 }
 
+function startLoadingImageReference(file: File): void {
+  const image = document.querySelector(CONTENT_IMAGE_REFERENCE) as
+    | HTMLImageElement
+    | null;
+  if (image) {
+    const context = new FileReader();
+    context.addEventListener(
+      "load",
+      (event: ProgressEvent<FileReader>) => {
+        const source = event.target?.result as string | null;
+        if (source) {
+          image.src = source;
+        }
+      },
+    );
+    context.readAsDataURL(file);
+  }
+}
+
+function setupImageReferenceElement(): void {
+  const image = document.querySelector(CONTENT_IMAGE_REFERENCE) as
+    | HTMLImageElement
+    | null;
+  if (image) {
+    image.addEventListener(
+      "load",
+      () => {
+        setupImageGeneratedElement();
+        (document.querySelector("#content-upload") as HTMLDivElement).style
+          .display = "none";
+        (document.querySelector("#content-images") as HTMLDivElement).style
+          .display = "inline";
+      },
+    );
+  }
+}
+
+function setupImageUploadElement(): void {
+  const input = document.querySelector(CONTENT_IMAGE_UPLOAD) as
+    | HTMLInputElement
+    | null;
+  if (input) {
+    input.addEventListener(
+      "change",
+      (event: Event) => {
+        const files = (event.target as HTMLInputElement | null)?.files;
+        if (files && files.length > 0) {
+          startLoadingImageReference(files[0]);
+        }
+      },
+    );
+  }
+}
+
+function setupImageGeneratedElement(): void {
+  const image = document.querySelector(CONTENT_IMAGE_REFERENCE) as
+    | HTMLImageElement
+    | null;
+  const canvas = document.querySelector(CONTENT_IMAGE_GENERATED) as
+    | HTMLCanvasElement
+    | null;
+  if (image && canvas) {
+    canvas.width = image.width;
+    canvas.height = image.height;
+
+    const context = canvas.getContext("webgl2");
+    if (context) {
+      // TODO: Launch the genetic algorithm!
+    } else {
+      throw new Error("WebGL2 isn't supported!");
+    }
+  }
+}
+
 self.addEventListener("load", () => {
-  setupPopulationSlider();
-  setupTrianglesSlider();
-  setupMutationSlider();
-  setupResolutionSlider();
+  setupImageUploadElement();
+  setupImageReferenceElement();
+
+  setupPopulationSliderElement();
+  setupTrianglesSliderElement();
+  setupMutationSliderElement();
+  setupResolutionSliderElement();
 });
