fd5d23ff60265887e9c4060f4f70dc81405857ba
tnosekk <nosektomek@gmail.com>
2024-11-22 22:06:45 +0100

    formated

--- client/src/crossover.ts
+++ client/src/crossover.ts
@@ -6,31 +6,32 @@
  * @returns { Float32Array } - Two offspring
  */
 export function crossover(
-	parent1: Float32Array,
-	parent2: Float32Array
+  parent1: Float32Array,
+  parent2: Float32Array,
 ): Float32Array {
-	const vertexSize = 6;
-	const vertexCount = parent1.length / vertexSize;
+  const vertexSize = 6;
+  const vertexCount = parent1.length / vertexSize;
 
-	if (parent1.length !== parent2.length || parent1.length % vertexSize !== 0){
-		throw new Error("Parents arrays must have the same length and be divisibe by the vertex size");
-	}
+  if (parent1.length !== parent2.length || parent1.length % vertexSize !== 0) {
+    throw new Error(
+      "Parents arrays must have the same length and be divisibe by the vertex size",
+    );
+  }
 
-	const crossoverPoint = Math.floor(Math.random() * vertexCount);
+  const crossoverPoint = Math.floor(Math.random() * vertexCount);
 
-	const offspring = new Float32Array(parent1.length);
+  const offspring = new Float32Array(parent1.length);
 
-	for (let i = 0; i < vertexCount; i++) {
-		for (let j = 0; j < vertexSize; j++){
-			const index = i * vertexSize + j;
-			
-			if (i < crossoverPoint){
-				offspring[index] = parent1[index];
-			} else {
-				offspring[index] = parent2[index];
-			}
-			}
-		}
-	return offspring;
-	}
+  for (let i = 0; i < vertexCount; i++) {
+    for (let j = 0; j < vertexSize; j++) {
+      const index = i * vertexSize + j;
 
+      if (i < crossoverPoint) {
+        offspring[index] = parent1[index];
+      } else {
+        offspring[index] = parent2[index];
+      }
+    }
+  }
+  return offspring;
+}
--- client/src/mutate.ts
+++ client/src/mutate.ts
@@ -9,40 +9,39 @@
  * @returns {Float32Array} - The mutated individual.
  */
 export function mutate(
-	vertices: Float32Array,
-	mutationRate: number,
-	population: number,
+  vertices: Float32Array,
+  mutationRate: number,
+  population: number,
 ): Float32Array {
+  for (let i = 0; i < vertices.length; i += 6) {
+    // random values for cordinates
+    if (Math.random() < mutationRate) {
+      vertices[i] = getRandom(population, i);
+    }
+    if (Math.random() < mutationRate) {
+      vertices[i + 1] = getRandom(population, i);
+    }
 
-	for (let i = 0; i < vertices.length; i += 6){
-		// random values for cordinates
-		if (Math.random() < mutationRate){
-			vertices[i] = getRandom(population,i); 
-		}
-		if (Math.random() < mutationRate) {
-			vertices[i+1] = getRandom(population,i);
-		}
-
-		// random value for colors
-		for (let j = 0; j < 4; j++){
-			if (Math.random() < mutationRate){
-				vertices[j+2+i] = Math.random();
-			}
-		}
-	 }
-	 return vertices;
+    // random value for colors
+    for (let j = 0; j < 4; j++) {
+      if (Math.random() < mutationRate) {
+        vertices[j + 2 + i] = Math.random();
+      }
+    }
+  }
+  return vertices;
 }
 /**
  * Generates a random value within a specific range
  * @param {number} population - size of population
- * @param {number} index - 
+ * @param {number} index -
  * @returns {number}
  */
 export function getRandom(
-	population: number,
-	index: number,
+  population: number,
+  index: number,
 ): number {
-	const range = 2/population;
-	const start = -1;
-	return Math.random() * (range) + (index + start);
+  const range = 2 / population;
+  const start = -1;
+  return Math.random() * range + (index + start);
 }
