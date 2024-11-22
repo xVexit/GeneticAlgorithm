8f37dd83dea79526b26b5b0c6236ad8c9dabf533
tnosekk <nosektomek@gmail.com>
2024-11-22 22:25:39 +0100

    updated crossover

--- client/src/crossover.ts
+++ client/src/crossover.ts
@@ -3,34 +3,27 @@
  *
  * @param {Float32Array} parent1 - The first parent
  * @param {Float32Array} parent2 - The secont parent
- * @returns { Float32Array } - Two offspring
+ * @returns { Float32Array } - offspring
  */
 export function crossover(
   parent1: Float32Array,
   parent2: Float32Array,
 ): Float32Array {
-  const vertexSize = 6;
-  const vertexCount = parent1.length / vertexSize;
-
-  if (parent1.length !== parent2.length || parent1.length % vertexSize !== 0) {
+  if (parent1.length !== parent2.length) {
     throw new Error(
       "Parents arrays must have the same length and be divisibe by the vertex size",
     );
   }
 
-  const crossoverPoint = Math.floor(Math.random() * vertexCount);
+  const crossoverPoint = Math.floor(Math.random() * parent1.length);
 
   const offspring = new Float32Array(parent1.length);
 
-  for (let i = 0; i < vertexCount; i++) {
-    for (let j = 0; j < vertexSize; j++) {
-      const index = i * vertexSize + j;
-
-      if (i < crossoverPoint) {
-        offspring[index] = parent1[index];
-      } else {
-        offspring[index] = parent2[index];
-      }
+  for (let i = 0; i < parent1.length; i++) {
+    if (i < crossoverPoint) {
+      offspring[i] = parent1[i];
+    } else {
+      offspring[i] = parent2[i];
     }
   }
   return offspring;
